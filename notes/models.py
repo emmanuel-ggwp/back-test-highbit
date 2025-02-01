ab3e9bede6fd47d0f28bac46e338ee9dd9303305
emmanuel-ggwp <emmanuel.marcano.gg@gmail.com>
2025-02-01 15:28:57 -0400

    Notes model created

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)  # Title of the note
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )  # Timestamp when the note is created
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )  # Timestamp when the note is last updated
+
+    def __str__(self):
+        return self.title
